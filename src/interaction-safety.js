d947e8020b482df00cd67c440402e64e2dd4203e
abdelrmamsaahlan-max <abdelrmamsaahlan@gmail.com>
2026-09-15 17:27:52 +0300

    Prevent transient Discord 503 gateway errors from crashing FSMM

--- src/interaction-safety.js
+++ src/interaction-safety.js
@@ -2,6 +2,25 @@
 const {Client}=require('discord.js');
 if(!global.__fsmmInteractionSafety){
   global.__fsmmInteractionSafety=true;
+  process.on('unhandledRejection',e=>console.error('[FSMM UNHANDLED REJECTION]',e?.stack||e));
+  process.on('uncaughtException',e=>{
+    const m=String(e?.message||e);
+    if(/Unexpected server response:\s*503/i.test(m)||/websocket/i.test(String(e?.stack||''))){
+      console.error('[FSMM GATEWAY] transient Discord websocket error; keeping process alive:',m);
+      return;
+    }
+    console.error('[FSMM UNCAUGHT]',e?.stack||e);
+    process.exitCode=1;
+  });
+  const originalLogin=Client.prototype.login;
+  Client.prototype.login=function(...args){
+    if(!this.__fsmmSafetyAttached){
+      this.__fsmmSafetyAttached=true;
+      this.on('error',e=>console.error('[FSMM DISCORD ERROR]',e?.stack||e));
+      this.on('shardError',e=>console.error('[FSMM SHARD ERROR]',e?.stack||e));
+    }
+    return originalLogin.apply(this,args);
+  };
   const originalEmit=Client.prototype.emit;
   Client.prototype.emit=function(event,...args){
     if(event==='interactionCreate'){
